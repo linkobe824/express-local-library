37b79fa2e0789e90445f44f8d695f01c18afdd2f
Manuel Beltran <manueldbeltranc@gmail.com>
2023-11-01 16:48:28 -0700

    add catalog route, dummy controllers for this route and layout

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ require('dotenv').config()
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const catalogRouter = require('./routes/catalog')
 
 const app = express()
 
 // set up mongoose connection
-const mongoose = reqire('mongoose')
+const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 const mongoDB = process.env.DB_URL
 
 main().catch((err) => console.log(err))
+
 async function main() {
-  await mongoose.connnect(mongoDB)
+  await mongoose.connect(mongoDB)
+  console.log('DB connected')
 }
 
 // view engine setup
@@ -32,14 +35,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+app.use('/catalog', catalogRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404))
 })
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
