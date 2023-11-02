7e149eeaf9576c0be4dbc0bc46357d94b768a574
Manuel Beltran <manueldbeltranc@gmail.com>
2023-11-01 18:16:44 -0700

    add author_list view and authors handler, also add virtual formatted dates to the author model

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const { DateTime } = require('luxon')
 
 const Schema = mongoose.Schema
 
@@ -19,6 +20,20 @@ AuthorSchema.virtual('name').get(function () {
 
 AuthorSchema.virtual('url').get(function () {
   return `/catalog/author/${this._id}`
+})
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+  if (this.date_of_birth)
+    return DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    )
+})
+
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+  if (this.date_of_death)
+    return DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    )
 })
 
 module.exports = mongoose.model('Author', AuthorSchema)
