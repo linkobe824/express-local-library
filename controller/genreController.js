8d63099615aa80353463e8fdf6fab2c70c1d47c0
Manuel Beltran <manueldbeltranc@gmail.com>
2023-11-02 18:35:00 -0700

    add genre_create_get and genre_create_post handlers and genre_form view

--- controller/genreController.js
+++ controller/genreController.js
@@ -1,6 +1,7 @@
 const Genre = require('../models/genre')
 const Book = require('../models/book')
 const asyncHandler = require('express-async-handler')
+const { body, validationResult } = require('express-validator')
 
 // Display list of all Genre.
 exports.genre_list = asyncHandler(async (req, res, next) => {
@@ -33,14 +34,54 @@ exports.genre_detail = asyncHandler(async (req, res, next) => {
 })
 
 // Display Genre create form on GET.
-exports.genre_create_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre create GET')
-})
+exports.genre_create_get = (req, res, next) => {
+  res.render('genre_form', { title: 'Create Genre' })
+}
 
 // Handle Genre create on POST.
-exports.genre_create_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre create POST')
-})
+// array de middlewares
+exports.genre_create_post = [
+  // valida y sanitiza el campo "name"
+  body('name', 'Genre name must containt at least 3 charactesr')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  // procesa peticion despues de la validacion y sanitizacion
+  asyncHandler(async (req, res, next) => {
+    // extrae los errores de valicacion de la request
+    const errors = validationResult(req)
+
+    // crea un objeto genre con la data sanitizada
+    const genre = new Genre({ name: req.body.name })
+
+    if (!errors.isEmpty()) {
+      // hay errores. Renderiza la forma con los valores sanitizados
+      // y mensajes de error
+      res.render('genre_form', {
+        title: 'Create Genre',
+        genre: genre,
+        errors: errors.array(),
+      })
+      return
+    } else {
+      // Los datos del formulario son validos.
+      // verifica si un Genre con el mismo nombre existe
+      const genreExists = await Genre.findOne({ name: req.body.name })
+        .collation({ locale: 'en', strength: 2 })
+        .exec()
+
+      if (genreExists) {
+        // si el Genre existe, redirige a su pagina de detalles
+        res.redirect(genreExists.url)
+      } else {
+        await genre.save()
+        // nuevo genero grabado. Redirige a la pagina de detalles
+        res.redirect(genre.url)
+      }
+    }
+  }),
+]
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
