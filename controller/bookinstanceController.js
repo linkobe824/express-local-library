5d62e8f9f11e684d6afbd9fd65f2ff6b34a19265
Manuel Beltran <manueldbeltranc@gmail.com>
2023-11-07 21:22:06 -0700

    fix an error with multiple pug tags

--- controller/bookinstanceController.js
+++ controller/bookinstanceController.js
@@ -107,10 +107,66 @@ exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display BookInstance update form on GET.
 exports.bookinstance_update_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance update GET')
+  const [bookInstance, allBooks] = await Promise.all([
+    BookInstance.findById(req.params.id).exec(),
+    Book.find().exec(),
+  ])
+
+  if (!bookInstance) {
+    const err = new Error('Book instance not found')
+    err.status = 404
+    next(err)
+  }
+
+  res.render('bookinstance_form', {
+    title: 'Update Book Instance',
+    bookinstance: bookInstance,
+    book_list: allBooks,
+  })
 })
 
 // Handle bookinstance update on POST.
-exports.bookinstance_update_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance update POST')
-})
+exports.bookinstance_update_post = [
+  body('book', 'Book must be specified').trim().isLength({ min: 1 }).escape(),
+  body('imprint', 'Imprint must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('status').escape(),
+  body('due_back', 'Invalid date')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+      _id: req.params.id,
+    })
+
+    if (!errors.isEmpty()) {
+      const allBooks = await Book.find({}, 'title').exce()
+
+      res.render('bookinstance_form', {
+        title: 'Update Book Instance',
+        book_list: allBooks,
+        selected_book: bookInstance.book._id,
+        errors: errors.array(),
+        bookisntance: bookInstance,
+      })
+      return
+    } else {
+      const updatedBookInstance = await BookInstance.findByIdAndUpdate(
+        req.params.id,
+        bookInstance,
+        {}
+      )
+      res.redirect(updatedBookInstance.url)
+    }
+  }),
+]
